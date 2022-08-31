c5d4cdb9925fd51f900316353822725ae8fc3bc6
Visa Aksnes <viniciussouzaavila@hotmail.com>
2022-08-31 13:24:57 -0300

    Upgrade geral

--- src/components/pages/static/navbar/menu/index.js
+++ src/components/pages/static/navbar/menu/index.js
@@ -1,13 +1,15 @@
 import Styles from './Menu.module.css'
 import {useState} from 'react';
 import {FaAlignJustify} from 'react-icons/fa';
-
+import {AiOutlineCloseCircle} from 'react-icons/ai'
+import './anima.css';
 export default function Menu({categoryModal,setCategoryModal}){
+     
     return(
         <>
-        {categoryModal==false?
+        {categoryModal===false?
             <ul className={Styles.menuNav}>
-                <li onClick={()=>{categoryModal?setCategoryModal(false):setCategoryModal(true)}} className={Styles.centralizarVertical}><FaAlignJustify/>Todas as categorias</li>
+                <li onClick={()=>setCategoryModal(true)} className={Styles.centralizarVertical}><FaAlignJustify/>Todas as categorias</li>
                 <li>Ofertas do dia</li>
                 <li>Moda feminino</li>
                 <li>Moda masculino</li>
@@ -15,7 +17,7 @@ export default function Menu({categoryModal,setCategoryModal}){
             </ul>
             :
             <ul className={Styles.closeMenuNav}>
-                <li onClick={()=>categoryModal?setCategoryModal(false):setCategoryModal(true)}>Fechar todas categorias</li>
+                <li onClick={()=>setCategoryModal(false)}><AiOutlineCloseCircle/>Fechar todas categorias</li>
             </ul>
             }
             {categoryModal&&
